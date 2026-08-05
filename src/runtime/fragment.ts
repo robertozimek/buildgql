f560db3c965a1f36b80121e9d92598ca28998cb4
buildql <buildql@local>
2026-08-05 15:29:27 -0400

    feat(runtime): query/mutation/subscription operation builders

---
+++ src/runtime/fragment.ts
@@ -0,0 +1,6 @@
+import type { Node } from '../types/node.js';
+import type { FragmentDef } from './print.js';
+
+export function collectFragments(_sels: readonly Node[]): FragmentDef[] {
+  return [];
+}
---
+++ test/types/operation.test-d.ts
@@ -0,0 +1,49 @@
+import { args, leaf, object, objectArgs } from '../../src/runtime/builders.js';
+import { makeMutation, makeQuery } from '../../src/runtime/operation.js';
+import { $ } from '../../src/runtime/var.js';
+import type { RESULT, VARS } from '../../src/types/symbols.js';
+
+type Expect<T extends true> = T;
+type Eq<A, B> = (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B ? 1 : 2 ? true : false;
+
+const User = {
+  id: leaf<'id', ['!'], string>('id', ['!']),
+  firstName: leaf<'firstName', ['!'], string>('firstName', ['!']),
+  lastName: leaf<'lastName', [], string>('lastName', []),
+};
+const Post = {
+  id: leaf<'id', ['!'], string>('id', ['!']),
+  title: leaf<'title', ['!'], string>('title', ['!']),
+  author: object('author', ['!'], User),
+};
+const Query = { posts: object('posts', ['!', 'l', '!'], Post) };
+const Mutation = {
+  createUser: objectArgs(
+    'createUser',
+    ['!'],
+    User,
+    args<{ name: string; email: string }>({ name: 'String!', email: 'String!' }),
+  ),
+};
+
+const query = makeQuery(Query);
+const mutation = makeMutation(Mutation);
+
+const q = query('Posts', ($, Q) => [
+  Q.posts((P) => [P.id, P.title, P.author((A) => [A.id, A.firstName, A.lastName])]),
+]);
+type _1 = Expect<
+  Eq<
+    NonNullable<(typeof q)[typeof RESULT]>,
+    { posts: { id: string; title: string; author: { id: string; firstName: string; lastName: string | null } }[] }
+  >
+>;
+type _2 = Expect<Eq<NonNullable<(typeof q)[typeof VARS]>, {}>>;
+
+const m = mutation('CreateNewUser', ($, M) => [
+  M.createUser({ name: $.name, email: $.email }, (U) => [U.id, U.firstName]),
+]);
+type _3 = Expect<Eq<NonNullable<(typeof m)[typeof VARS]>, { name: string; email: string }>>;
+type _4 = Expect<Eq<NonNullable<(typeof m)[typeof RESULT]>, { createUser: { id: string; firstName: string } }>>;
+
+export { q, m };
