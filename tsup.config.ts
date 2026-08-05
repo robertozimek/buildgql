5d52ca43e6a906447c0ae5a0b32b153cb1430094
Robert Ozimek <robertozimek90@gmail.com>
2026-08-05 19:53:04 -0400

    build: publish buildql/adapters/apollo and buildql/adapters/urql entry points

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/client/client.ts', 'src/cli/config.ts', 'src/cli/index.ts'],
+  entry: [
+    'src/index.ts',
+    'src/client/client.ts',
+    'src/cli/config.ts',
+    'src/cli/index.ts',
+    'src/adapters/apollo.ts',
+    'src/adapters/urql.ts',
+  ],
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
